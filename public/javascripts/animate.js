dcb3334b6d4e6c61556909602c65eca251f22f08
Poornesh4559 <poorneshofficial@gmail.com>
2022-02-15 16:08:22 +0530

    added projects in the index

--- public/javascripts/animate.js
+++ public/javascripts/animate.js
@@ -12,10 +12,10 @@ const popup = document.querySelectorAll('.fadeout')
 
 $('.anim').each(function(){
 //  let fadeup =  $(this).find('.fadeout')
-const textAnim = TweenMax.fromTo($(this),2, { opacity: 0 }, { opacity: 1 });
-const textTrans = TweenMax.to($(this),1,{css:{transform:'Translate(0px,-10px)'}})
+const textAnim = TweenMax.fromTo($(this),1, { opacity: 0 }, { opacity: 1 });
+//const textTrans = TweenMax.to($(this),1,{css:{transform:'Translate(0px,-10px)'}})
 
-timeline.add(textAnim).add(textTrans)
+timeline.add(textAnim)//.add(textTrans)
 
 let scene =  new ScrollMagic.Scene({
   triggerElement: this,
@@ -57,3 +57,25 @@ function blink_arrow() {
 
 
 setInterval(blink_arrow, 1000);
+
+
+
+function wrapTog(){
+  let ele = document.querySelector('.wrap-in')
+  if (ele.style.height === '40vh'){
+    ele.style.transition = "height 0.2s ease-in"
+    ele.style.height = "2px"
+  }else{
+    ele.style.transition = "height 0.4s ease-in"
+    ele.style.height = "40vh" 
+  }
+}
+
+function rotate180(){
+  let ele = document.querySelector('.wrap-arrow')
+  if (ele.style.transform === 'rotate(180deg)'){
+    ele.style.transform ='rotate(0deg)'
+  }else{
+    ele.style.transform ='rotate(180deg)'
+  }
+}
